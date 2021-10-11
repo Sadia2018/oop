b94ea6b304b7f938397b2ba32616d03984fc8faa
Sadia_S <37849950+Sadia2018@users.noreply.github.com>
2021-10-10 22:44:46 -0700

    bank account

---
+++ bank_account.py
@@ -0,0 +1,18 @@
+
+class BankAccount:
+
+  def __init__(self, int_rate, balance): 
+      self.int_rate = int_rate
+      self.balance = 0
+
+def deposit(self, amount):
+    self.amount += amount
+
+def withdraw(self, amount):
+    self.amount -= amount
+
+def display_account_info(self):
+    self.display_account_info = display_account_info
+
+def yield_interest(self):
+    self.yield_interest += yield_interest
