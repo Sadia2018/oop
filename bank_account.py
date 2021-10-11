1b95e68bd809f98b8d1de4f8d99de80a11cfd886
Sadia_S <37849950+Sadia2018@users.noreply.github.com>
2021-10-10 23:06:11 -0700

    made deposits in account

--- bank_account.py
+++ bank_account.py
@@ -1,18 +1,41 @@
-
 class BankAccount:
+    accounts = []
+    def __init__(self,int_rate,balance):
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.accounts.append(self)
 
-  def __init__(self, int_rate, balance): 
-      self.int_rate = int_rate
-      self.balance = 0
+    def deposit(self, amount):
+        self.balance += amount
+        return self
 
-def deposit(self, amount):
-    self.amount += amount
+    def withdraw(self,amount):
+        if(self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+    
+    def display_account_info(self):
+        print(f"Balance: {self.balance}")
+        return self
 
-def withdraw(self, amount):
-    self.amount -= amount
+    def yeild_interest(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
+        return self
 
-def display_account_info(self):
-    self.display_account_info = display_account_info
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
 
-def yield_interest(self):
-    self.yield_interest += yield_interest
+
+savings = BankAccount(.09,1000)
+checking = BankAccount(.02,5000)
+
+savings.deposit(10).deposit(20).deposit(40).withdraw(600).yeild_interest().display_account_info()
+checking.deposit(100).deposit(200).deposit(400).withdraw(60).yeild_interest().display_account_info()
+
+BankAccount.print_all_accounts()
